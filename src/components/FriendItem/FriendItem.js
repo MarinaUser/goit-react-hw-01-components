50ccf766d01d24c0ee30cba608c427882701c6fa
Marina Bibichenko <108667532+MarinaUser@users.noreply.github.com>
2023-01-28 12:27:04 +0200

    style for friends

--- src/components/FriendItem/FriendItem.js
+++ src/components/FriendItem/FriendItem.js
@@ -4,7 +4,7 @@ import css from './FriendItem.module.css';
 export const FriendItem = ({ name, avatar, isOnline }) => {
   return (
     <li className={css.item}>
-      <span className={isOnline ? css.online : css.offline}>{isOnline}</span>
+      <span className={isOnline ? css.on : css.off}>{isOnline}</span>
       <img className={css.avatar} src={avatar} alt="User avatar" width="60" />
       <p className={css.name}>{name}</p>
     </li>
